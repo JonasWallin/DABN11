11ac3857e020b45274d28205fa39fffa84705127
JonasWallin <jonas.wallin81@gmail.com>
2023-08-29 11:23:54 +0200

    update file

--- assignment/lab1/task_4.R
+++ assignment/lab1/task_4.R
@@ -5,12 +5,12 @@ test_that("Marking of exercise 4", {
   set.seed(6)
   u <- runif(6)
   u.data.frame.true <- data.frame("name" = 1:6, "value" = u)
-  expect_equal(name(u.data.frame), name(u.data.frame.true), info = "u.data.frame contains column names")
+  expect_equal(colnames(u.data.frame), colnames(u.data.frame.true), info = "u.data.frame contains column names")
   expect_equal(u.data.frame, u.data.frame.true, info = "u.data.frame contains erroneous values")
     
     
   u.tibble.true <- enframe(u) 
-  expect_equal(name(u.tibble), name(u.tibble.true), info = "u.tibble contains column names")
+  expect_equal(names(u.tibble), names(u.tibble.true), info = "u.tibble contains column names")
   expect_equal(u.tibble, u.tibble.true, info = "u.tibble contains erroneous values")
 
 })
