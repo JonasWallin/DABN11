ffb2984f1634fcdd702883c54536fd36d5601b38
JonasWallin <jonas.wallin81@gmail.com>
2023-08-29 12:13:58 +0200

    update file

--- assignment/lab1/task_3.R
+++ assignment/lab1/task_3.R
@@ -4,7 +4,7 @@ test_that("Marking of exercise 3", {
   set.seed(4)
   dices.true <- sample(1:6,100, replace = T)
   dices.true.6 = sum(dices.true==6)
-  expect_equal(dices, dices.true, info = "dices contains erroneous values")
-  expect_equal(dices.is.6, dices.true.6, info = "dices.is.6 contains erroneous values")
+  expect_equal(TRUE, compare(dices,dices.true)$equal, info = "\ndices contains erroneous values")
+  expect_equal(TRUE, compare(dices.is.6,dices.true.is.6)$equal, info = "\ndices.is.6 contains erroneous values")
 
 })
