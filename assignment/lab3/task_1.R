43a673660087e5eb508a3b7fb11be08dc6859212
JonasWallin <jonas.wallin81@gmail.com>
2023-09-04 11:39:58 +0200

    file update lab3

--- assignment/lab3/task_1.R
+++ assignment/lab3/task_1.R
@@ -4,6 +4,6 @@ context("task 1")
 
 test_that("task 1", {
     expect_true(exists("data"), "Variable data is missing")
-    data_test <- readRDS(url("https://github.com/JonasWallin/DABN11/blob/main/assignment/lab3/task1.RDS"))
+    data_test <- readRDS(url("https://github.com/JonasWallin/DABN11/raw/main/assignment/lab3/task1.RDS"))
     expect_equal(data, data_test,label="data is nos not correct")
 })
